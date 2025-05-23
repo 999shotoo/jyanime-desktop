60675f3f72d0449c752370934cef4ff934ebace7
999shotoo <mohitshedge5@gmail.com>
2025-05-24 00:02:20 +0530

    test online / offline mode

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
 import Home from './pages/home';
 import Work from './pages/work';
 import Header from './components/headerControls';
 import { ThemeProvider } from './components/themeProvider';
 
-const Layout: React.FC = () => (
-  <div>
-    <Header />
-    <nav className='pt-10'>
-      <Link to="/">Home</Link> | <Link to="/work">Work</Link>
-    </nav>
-    <Outlet />
-  </div>
-);
 
+
+const Layout = () => {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  const handleOnline = () => {
+    setIsOnline(true);
+  };
+
+  const handleOffline = () => {
+    setIsOnline(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
+    };
+  }, []);
+
+  return (
+    <>
+      <div>
+        <Header />
+        <nav className='pt-10'>
+          <Link to="/">Home</Link> | <Link to="/work">Work</Link>
+        </nav>
+        <div>
+            <p>You are currently {isOnline ? 'online' : 'offline'}</p>
+        </div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
 
 
 function App(): React.JSX.Element {
